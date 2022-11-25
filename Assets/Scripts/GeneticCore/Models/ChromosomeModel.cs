577994b3bf851e19dc467a5823347a049122b513
aryana <a.peyrovi@medrick.info>
2022-11-25 05:12:38 +0330

    Add .idea folder to gitignore file, Create chromosome and genoms models.

---
+++ Assets/Scripts/GeneticCore/Models/ChromosomeModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GeneticCore
+{
+    public class ChromosomeModel
+    {
+        public readonly Dictionary<string, GenomeModel> Data;
+        public int Score;
+        
+        public ChromosomeModel(List<GenomeModel> genomeModels)
+        {
+            Data = new Dictionary<string, GenomeModel>();
+            foreach (var genome in genomeModels)
+            {
+                Data[genome.Tag] = genome;
+            }
+        }
+
+        public void SetScore(int score)
+        {
+            Score = score;
+        }
+
+        public void PrintData()
+        {
+            var line = "";
+            foreach (var pair in Data)
+            {
+                line += $"{pair.Key}: {pair.Value.Value}, ";
+            }
+            Debug.Log(line);
+        }
+    }
+}
---
+++ Assets/Scripts/GeneticCore/Models/GenomeModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace GeneticCore
+{
+    public class GenomeModel
+    {
+        public string Tag;
+        public int Value;
+        
+        public GenomeModel(string tag, int value)
+        {
+            Tag = tag;
+            Value = value;
+        }
+
+        public void AddValue()
+        {
+            Value *= 1000;
+        }
+    }
+}
