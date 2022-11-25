403e7b2a5edb746ea129e6a565e28f71b9fad10f
aryana <a.peyrovi@medrick.info>
2022-11-25 20:34:49 +0330

    Addd test for linq select.

--- Assets/Scripts/GeneticAlgorithm/Core/Tests/TestModel.cs
+++ Assets/Scripts/GeneticAlgorithm/Core/Tests/TestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
-using GeneticAlgorithm;
+using System.Linq;
+using GeneticAlgorithm.Core.Evaluators;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -12,6 +13,7 @@ namespace GeneticAlgorithm.Core
         public void DoTests()
         {
             Test1ChangingOneGeneValuesShouldNotChangeOthers();
+            Test2LinqSelect();
         }
         
         private void Test1ChangingOneGeneValuesShouldNotChangeOthers()
@@ -39,6 +41,39 @@ namespace GeneticAlgorithm.Core
                 return;
             }
             Debug.Log($"<color=#00FF00> Test1 Successfully Passed. </color>");
+        }
+        
+        private void Test2LinqSelect()
+        {
+            var chrmsmA = new ChromosomeModel(new List<GenomeModel>
+            {
+                new GenomeModel("X", 5),
+                new GenomeModel("Y", 10),
+                new GenomeModel("Z", 15),
+                new GenomeModel("W", 20),
+            });
+            chrmsmA.SetScore(0);
+            var chrmsmB = new ChromosomeModel(new List<GenomeModel>
+            {
+                new GenomeModel("X", -5),
+                new GenomeModel("Y", -10),
+                new GenomeModel("Z", -15),
+                new GenomeModel("W", -20),
+            });
+            chrmsmB.SetScore(0);
+            var i = new List<ChromosomeModel>();
+            i.Add(chrmsmA);
+            i.Add(chrmsmB);
+            var x = new Evaluator();
+            var score1 = i[0].Score;
+            i.Select(Container.Evaluator.EvaluateChromosome).ToList();
+            var score2 = i[0].Score;
+            if (score1 == score2)
+            {
+                Debug.Log($"<color=#FF0000> Test2 Failed. </color>");
+                return;
+            }
+            Debug.Log($"<color=#00FF00> Test2 Successfully Passed. </color>");
         }
     }
 }
