ccf904656c0aad168443843280479eb474181e92
aryana <a.peyrovi@medrick.info>
2022-11-25 05:13:52 +0330

    Add chromosome factory, Implement crossovering between two parent chromosomes logic, Add tests,

---
+++ Assets/Scripts/GeneticCore/Logics/ChromosomeFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneticCore.Logics
+{
+    public static class ChromosomeFactory
+    {
+        public static (ChromosomeModel offSpringA, ChromosomeModel offSpringB) CreateOffSpringChromosomes(ChromosomeModel parentA, ChromosomeModel parentB, float crossOverOffset = 0.5f)
+        {
+            var genomesOfParentA = parentA.Data.Values.ToList();
+            var genomesOfParentB = parentB.Data.Values.ToList();
+            if (genomesOfParentA.Count != genomesOfParentB.Count)
+            {
+                throw new Exception("[ChromosomeFactory]: Size Of Chromosomes Are Not Equal.");
+            }
+
+            var sizeOfChromosomes = genomesOfParentA.Count;
+            var crossOverPoint = (int) (crossOverOffset * sizeOfChromosomes);
+
+            var genomesOfChildA = new List<GenomeModel>();
+            var genomesOfChildB = new List<GenomeModel>();
+
+            for (int i = 0; i < crossOverPoint; i++)
+            {
+                genomesOfChildA.Add(CloneGenome(genomesOfParentB[i]));
+                genomesOfChildB.Add(CloneGenome(genomesOfParentA[i]));
+            }
+
+            for (int i = crossOverPoint; i < sizeOfChromosomes; i++)
+            {
+                genomesOfChildA.Add(CloneGenome(genomesOfParentA[i]));
+                genomesOfChildB.Add(CloneGenome(genomesOfParentB[i]));
+            }
+
+            var childA = new ChromosomeModel(genomesOfChildA);
+            var childB = new ChromosomeModel(genomesOfChildB);
+            // Children are called offSprings too
+            return (childA, childB);
+        }
+
+        private static GenomeModel CloneGenome(GenomeModel genome)
+        {
+            return new GenomeModel(genome.Tag, genome.Value);
+        }
+    }
+}
---
+++ Assets/Scripts/GeneticCore/Tests/TestModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using GeneticCore.Logics;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GeneticCore
+{
+    [CreateAssetMenu(fileName = "GeneticCoreTests", menuName = "Tests/StructureTest", order = 0)]
+    public class TestModel : ScriptableObject
+    {
+        [Button]
+        public void DoTests()
+        {
+            Test1ChangingOneGeneValuesShouldNotChangeOthers();
+        }
+        
+        private void Test1ChangingOneGeneValuesShouldNotChangeOthers()
+        {
+            var chrmsmA = new ChromosomeModel(new List<GenomeModel>
+            {
+                new GenomeModel("X", 5),
+                new GenomeModel("Y", 10),
+                new GenomeModel("Z", 15),
+                new GenomeModel("W", 20),
+            });
+            var chrmsmB = new ChromosomeModel(new List<GenomeModel>
+            {
+                new GenomeModel("X", -5),
+                new GenomeModel("Y", -10),
+                new GenomeModel("Z", -15),
+                new GenomeModel("W", -20),
+            });
+            var (chrmsmC, chrmsmD) = ChromosomeFactory.CreateOffSpringChromosomes(chrmsmA, chrmsmB, 0.5f);
+            chrmsmC.Data["X"].AddValue();
+            var isChangesToo = chrmsmC.Data["X"].Value == chrmsmB.Data["X"].Value;
+            if (isChangesToo)
+            {
+                Debug.Log($"<color=#FF0000> Test1 Failed. </color>");
+                return;
+            }
+            Debug.Log($"<color=#00FF00> Test1 Successfully Passed. </color>");
+        }
+    }
+}
