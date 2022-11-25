51b8847006429d91376b393c71f4156b7b4c2231
aryana <a.peyrovi@medrick.info>
2022-11-26 00:58:20 +0330

    Add main class to connect user to algorithm.

---
+++ Assets/Scripts/GeneticAlgorithm/Main.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeneticAlgorithm.Core;
+using GeneticAlgorithm.Core.Evaluators;
+
+namespace GeneticAlgorithm
+{
+    [System.Serializable]
+    public class GeneticAlgorithmInfo
+    {
+        public int pairFactor = 1;
+        public float crossOverOffset = 0.5f;
+        public float mutationChance = 0.1f;
+        public float genomeMutateChance = 0.5f;
+    }
+    public class GeneticAlgorithm
+    {
+        public GeneticAlgorithm(int chromosomesGeneCount, int initialPeopleCount, IEvaluator evaluator)
+        {
+            var evaluatorController = new EvaluatorController(evaluator);
+            var populationManager = new PopulationManager(chromosomesGeneCount, initialPeopleCount);
+        }
+
+        public GeneticAlgorithm SetGeneticAlgorithmStructuralParameters(GeneticAlgorithmInfo info)
+        {
+            Container.PopulationManager.SetPairFactor(info.pairFactor)
+                .SetCrossOverOffset(info.crossOverOffset)
+                .SetMutationChance(info.mutationChance)
+                .SetGenomeMutationChance(info.genomeMutateChance);
+            return this;
+        }
+
+        public List<short> RunAlgorithm()
+        {
+            var winnerChromosome = Container.PopulationManager.Build();
+            return winnerChromosome.Data.Select(item => item.Value).ToList();
+        }
+    }
+}
